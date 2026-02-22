f5a9f48de3e4d16ab63523b26c137c1c795dc4ea
Juergen Mueller <juergen.mueller@astrid-lindgren-zentrum.at>
2026-02-22 22:44:17 +0100

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -29,6 +30,7 @@ function App() {
           </Routes>
         </main>
         <Footer />
+        <Analytics />
       </div>
     </Router>
   );
